f8eb17f2b7a623d4bd7d87cf77a64cf6a5644800
Andres Cecilia Luque <a.cecilia.luque@gmail.com>
2021-10-11 22:59:36 +0100

    Initial commit

---
+++ test.sh
@@ -0,0 +1,12 @@
+#!/bin/zsh
+
+set -euo pipefail
+
+sudo /usr/bin/dscl . -passwd /Users/root "mypass"
+# sudo security set-keychain-password -o oldpassword -p newpassword /Users/username/Library/Keychains/login.keychain
+
+echo "mypass" | sudo -v -S
+
+sudo echo "something1"
+/bin/bash -c "/usr/bin/sudo echo something2"
+/bin/bash -c "if ! /usr/bin/sudo echo something3;then echo something4;fi"
